ccbe508e9174da4699e51b13db31c2c2daeaf7a3
Ian Morrissey <77222437+Cardguy1@users.noreply.github.com>
2025-12-03 02:21:24 -0600

    updated

--- Food Log App/Food-Log-App/app/(tabs)/_layout.tsx
+++ Food Log App/Food-Log-App/app/(tabs)/_layout.tsx
@@ -1,7 +1,51 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { HapticTab } from '@/components/haptic-tab';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export default function SettingsLayout() {
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
   return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}>
+      <Tabs.Screen
+        name="Home Page"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Calendar"
+        options={{
+          title: 'Calendar',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="calendar" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+/*
+This is Oyins code for a tabs layout. I have no idea what it does. Please let us know or try to make it work with the tabs
+
+return (
     <Stack
       screenOptions={{
         headerShown: false,
@@ -10,4 +54,4 @@ export default function SettingsLayout() {
       }}
     />
   );
-}
+*/
