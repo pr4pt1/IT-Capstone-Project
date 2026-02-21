650794929b1acdfb0d8755d4f8bba8b1584bdfd4
Prapti <pyakurelp.s@gmail.com>
2026-02-20 19:24:38 -0600

    home page containers
    added containers for inputted data

--- Food Log App/Food-Log-App/app/(tabs)/Home Page.tsx
+++ Food Log App/Food-Log-App/app/(tabs)/Home Page.tsx
@@ -26,7 +26,23 @@ export default function WelcomePage() {
         />
         <Text style={styles.title}>Welcome Back</Text>
       </ThemedView>
-    </ThemedView>
+        
+      <ThemedView style={styles.cardsSection}>
+
+          <Pressable style={styles.card}>
+            <Text style={styles.cardTitle}>Today's Calories</Text>
+            <Text style={styles.cardValue}>6,240</Text>
+            <Text style={styles.cardSubtitle}>Your fat</Text>
+          </Pressable>
+
+          <Pressable style={styles.card}>
+            <Text style={styles.cardTitle}>Weekly Progress</Text>
+            <Text style={styles.cardSubtitle}>View trends & analytics</Text>
+          </Pressable>
+
+        </ThemedView>
+
+      </ThemedView>
     </ImageBackground>
   );
 }
@@ -60,7 +76,44 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: 32,
+    color: '#1C1C1E',
     fontWeight: 'bold',
     fontFamily: 'Nunito'
+  },
+
+  cardsSection: {
+    padding: 10,
+    gap: 20,
+    backgroundColor: 'transparent',
+  },
+
+  card: {
+    backgroundColor: '#1C1C1E', 
+    padding: 22,
+    borderRadius: 24,
+    borderWidth: 1,
+    borderColor: '#2C2C2E',
+    shadowColor: '#000',
+    shadowOpacity: 0.4,
+    shadowRadius: 15,
+    elevation: 8,
+  },
+
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#ffffff',
+    marginBottom: 8,
+  },
+
+  cardSubtitle: {
+    fontSize: 14,
+    color: '#A1A1A6',
+  },
+
+  cardValue: {
+    fontSize: 34,
+    fontWeight: '700',
+    color: '#636B2F', // modern green accent
   },
 });
